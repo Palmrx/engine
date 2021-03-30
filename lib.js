4850ff915ccd3ebb9ba107077f558c1696790066
Rodney-Thomas <80913258+Rodney-Thomas@users.noreply.github.com>
2021-03-30 01:42:57 -0500

    Update lib.js

--- lib.js
+++ lib.js
@@ -2,7 +2,10 @@ function palm(eid) {
   const engine =
   {
     log:(logging)=>{
-      console.log(logging)
+      console.log(logging);
+    },
+    alert:(alerting)=>{
+      alert("Palm Engine" + "\n" + alerting);
     }
   }
 }
